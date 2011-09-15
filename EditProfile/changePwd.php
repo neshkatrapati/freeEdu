e49f6160aa9303d776a79a13835c36898f90527c
Aditya <aditya@Aditya.(none)>
2011-09-15 23:16:32 +0530

    Check Add Later

--- EditProfile/changePwd.php
+++ EditProfile/changePwd.php
@@ -3,10 +3,8 @@
 </head>
 <body>
 <?php
-include("connection.php");
-
 echo "<form action='#' method='post'>";
-echo "Old Password*:<input type='password' name='opwd'><br>";
+echo "Old Password*:<input type='password' name='opswd'><br>";
 echo "New Password*:<input type='password' name='npwd'><br>";
 echo "Confirm Password*:<input type='password' name='cpwd'><br>";
 echo "<h4>Note:Please type the password minimum of 6 Charecters!</h4>";
@@ -14,32 +12,37 @@ echo "<input type='submit' name='pwd'>";
 echo "</form>";
 if(isset($_POST['pwd']))
 {
-	
+	include("../lib/connection.php");
 	$oid = $_COOKIE['object'];
-	$opwd=$_POST['opwd'];
+	$opswd=$_POST['opswd'];
 	$npwd=$_POST['npwd'];
 	$cpwd=$_POST['cpwd'];
+	echo $npwd;
+	echo $opswd;
+	echo $cpwd;
 	$len=strlen($npwd);
 	$pawd=mysql_query("select * from MOBJECTT where oid='$oid'");
 	while($pass=mysql_fetch_array($pawd))
 	{	
 		$oldpwd=$pass['opwd'];
 	}
-	if($opwd==$oldpwd)
+	echo "1.".$opswd;
+	echo "2.".$oldpwd."kofda";
+	if($opswd==$oldpwd)
 	{
-		if($npwd==$cpwd && $len<6)
-		{
-			echo "<h3>The new password is less than 6 letters!</h3>";
-		}	
-		else if($npwd==$cpwd && $npwd!=NULL)
-		{
-			mysql_query("update MOBJECTT set opwd='$npwd' where oid='$oid'");
-			echo "<script type='text/javascript'>parent.location.reload(1);</script>";
-		}
-		else if($npwd==$cpwd && $npwd==NULL)
+		if($npwd==$cpwd && $len==0)
 		{
 			echo "<h3>Some fields are empty. Please fill them!</h3>";
 			
+		}
+		else if($npwd==$cpwd && $len<6)
+		{
+			echo "<h3>The new password is less than 6 letters!</h3>";
+		}	
+		else if($npwd==$cpwd && $npwd!=NULL && $len>5)
+		{
+			mysql_query("update MOBJECTT set opwd='$npwd' where oid='$oid'");
+			echo "<script type='text/javascript'>parent.location.reload(1);</script>";
 		}
 		else
 		{
@@ -54,6 +57,9 @@ if(isset($_POST['pwd']))
 	else
 	{
 		echo "<h3>Authentication Failure</h3>";
+		echo "1234".$oldpwd;
+		echo "hello";
+		echo $oid;
 	}
 
 }
---
+++ misc/addLateral.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+
+</head>
+<body>
+    <fieldset>
+    	<legend>Choose Batch For Analysis</legend>
+   
+        <?php
+        include("../lib/lib.php");
+        include("../lib/connection.php");
+        echo "<center>";
+	echo "<form action='#' method='post' >";
+        $placeBranch = "";
+	$placeBranch = getBatches('bat[]');
+	echo "Select A Batch:".$placeBranch;
+	echo "<input type='submit' name='submit' value='submit'/>";
+        ?>
+    </form>
+    </fieldset>
+    </center>
+</body>
+</html>
